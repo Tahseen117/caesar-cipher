c4dff6672aaf892e87c3f5af99b16bc3606f1762
Tahseen117 <tahskhan117@gmail.com>
2022-06-08 18:45:20 +0530

    Added feature to apply shift greater than 26*2.
    Also added feature to include spaces and special characters

--- cipher.py
+++ cipher.py
@@ -5,18 +5,23 @@ def encoder (string, shift):
 
     encoded_word = ""
     for letter in string:
-        if letter.isupper():
-            output_letter = ord(letter) + shift
-            if output_letter > 90:
-                output_letter -= 26
 
-        elif letter.islower():
-            output_letter = ord(letter) + shift
-            if output_letter > 122:
-                output_letter -= 26
+        if (ord(letter) in range (65,91)) or (ord(letter) in range (97,123)):
 
-        encoded_word += chr(output_letter)
-    
+            if letter.isupper():
+                output_letter = ord(letter) + shift
+                if output_letter > 90:
+                    output_letter -= 26
+
+            elif letter.islower():
+                output_letter = ord(letter) + shift
+                if output_letter > 122:
+                    output_letter -= 26
+
+            encoded_word += chr(output_letter)
+        else:
+            encoded_word += letter
+
     print (encoded_word)
 
 
@@ -24,18 +29,22 @@ def decoder (string, shift):
 
     decoded_word = ""
     for letter in string:
-        if letter.isupper():
-            output_letter = ord(letter) - shift
-            if output_letter < 65:
-                output_letter += 26
-        
-        elif letter.islower():
-            output_letter = ord(letter) - shift
-            if output_letter < 97:
-                output_letter += 26
+        if (ord(letter) in range (65,91)) or (ord(letter) in range (97,123)):
 
-        decoded_word += chr(output_letter)
-    
+            if letter.isupper():
+                output_letter = ord(letter) - shift
+                if output_letter < 65:
+                    output_letter += 26
+            
+            elif letter.islower():
+                output_letter = ord(letter) - shift
+                if output_letter < 97:
+                    output_letter += 26
+
+            decoded_word += chr(output_letter)
+        else:
+            decoded_word += letter
+
     print (decoded_word)
 
 if __name__ == "__main__":
@@ -50,6 +59,7 @@ if __name__ == "__main__":
     encode_decode = input ("Enter the operation to be performed (encode/decode)\n")
     input_message = input("Enter the message to be encoded/decode :")
     shift = int(input("Enter the shift :"))
+    shift = shift % 26
 
     if encode_decode.lower() == 'encode':
         encoder(string=input_message, shift=shift)
